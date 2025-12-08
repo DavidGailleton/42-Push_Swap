b26780671b058011605985b91fada4381d4cde19
David Gailleton <dgaillet@z4r9p4.42lyon.fr>
2025-12-08 14:26:13 +0100

    struct define + mkdir stack functions folder

---
+++ push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgaillet <dgaillet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 14:18:06 by dgaillet          #+#    #+#             */
+/*   Updated: 2025/12/08 14:23:40 by dgaillet         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_stack
+{
+	int		value;
+	s_stack	*next;
+	s_stack	*previous;
+}	t_stack;
+
+#endif
