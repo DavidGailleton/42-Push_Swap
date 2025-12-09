c9066f4a9bb2eec53ad8507ab12c55c7f0329154
Maoake Teriierooiterai <mteriier@z4r8p3.42lyon.fr>
2025-12-09 12:32:13 +0100

    Algorithm firsts utils (stack size + check order)

---
+++ algorithms/utils/check_order.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_order.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mteriier <mteriier@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 11:57:06 by mteriier          #+#    #+#             */
+/*   Updated: 2025/12/09 12:02:28 by mteriier         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	check_order(t_stack *stack)
+{
+	t_stack	*first;
+
+	first = stack;
+	while (stack->next != first)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (0);
+}
